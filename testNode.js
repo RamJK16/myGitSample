3a609424dfbd92827cd17f7980f0c8dce2713eb2
RamJK16 <47057626+RamJK16@users.noreply.github.com>
2019-08-15 00:27:19 +0530

    Add files via upload

---
+++ testNode.js
@@ -0,0 +1,12 @@
+var http = require('http');
+
+http.createServer(function (req,res) {
+  console.log('Hello World from Console');
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(3000, "127.0.0.1");
+
+console.log('Server running at http://127.0.0.1:3000/');
+console.log('Added to git');
+console.log('Added to git2');
+console.log('Added to branch');
